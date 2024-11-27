f2d3935f2b71769efbf4c7850f0363ae7c7e11a7
isaac hershenson <ihershenson@hmc.edu>
2024-11-27 13:18:45 -0800

    url change

--- evals/test_agent.py
+++ evals/test_agent.py
@@ -15,7 +15,7 @@ FUZZY_MATCH_FIELDS = ("Role","Company")
 LIST_OF_STRING_FIELDS = ("Prior-Companies",)
 DEFAULT_DATASET_NAME = "Person Researcher Dataset"
 DEFAULT_GRAPH_ID = "people_maistro"
-DEFAULT_AGENT_URL = "https://ht-abandoned-cynic-27-d4d35e0b052a570a9c5cb83f703881f4.default.us.langgraph.app"
+DEFAULT_AGENT_URL = "https://api.smith.langchain.com/marketplace/cc9aac58-f334-4545-80d9-59300faf8aa2"
 
 os.environ["OPENAI_API_KEY"] = "..."
 os.environ["LANGSMITH_API_KEY"] = "..."
